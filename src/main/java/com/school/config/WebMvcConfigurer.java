59336a9aaf7d8043e7c86e9a42c9f722a4f1bcd6
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2020-10-10 01:28:58 +0100

    Swagger Config

---
+++ src/main/java/com/school/config/WebMvcConfigurer.java
@@ -0,0 +1,7 @@
+package com.school.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+}
