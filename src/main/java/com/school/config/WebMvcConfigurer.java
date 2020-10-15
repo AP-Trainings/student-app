48775876cfc1ee2104c3fdb1cbacc3509ad3cd38
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2020-10-15 15:57:11 +0100

    asdasd

--- src/main/java/com/school/config/WebMvcConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.school.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
-
-}
